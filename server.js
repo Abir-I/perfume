59fabfaa3bd3ca27b6e233d686bf8915d5130863
Rubaiya Zaman Mysha <rubaiyazamanmysha@gmail.com>
2026-07-02 12:33:30 +0600

    server.js

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const app = express();
 
-const productsRouter = require('./routes/products');   // Task 8
-const productRouter = require('./routes/product');      // Task 9
-const brandsRouter = require('./routes/brands');         // Task 10
+const productsRouter = require('./routes/products');   
+const productRouter = require('./routes/product');      
+const brandsRouter = require('./routes/brands');         
 
 app.use(express.json());
 
-app.use('/api/products', productsRouter); // GET /api/products?brand=&size=&type=
-app.use('/api/products', productRouter);  // GET /api/products/:id
-app.use('/api/brands', brandsRouter);     // GET /api/brands
+app.use('/api/products', productsRouter); 
+app.use('/api/products', productRouter);  
+app.use('/api/brands', brandsRouter);     
 
 app.get('/', (req, res) => {
   res.send('Perfume Store API is running. Try /api/products, /api/products/1, /api/brands');
